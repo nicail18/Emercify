1a7fd315051ed356c8bd7537dbd34264f4bbde32
BonJoven <joven.montanez06@gmail.com>
2018-11-29 21:56:19 +0800

    Glide

--- app/src/main/java/nicail/bscs/com/emercify/Likes/LikesActivity.java
+++ app/src/main/java/nicail/bscs/com/emercify/Likes/LikesActivity.java
@@ -13,18 +13,26 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
+import de.hdodenhof.circleimageview.CircleImageView;
 import nicail.bscs.com.emercify.R;
 import nicail.bscs.com.emercify.Utils.BottomNavigationViewHelper;
+import nicail.bscs.com.emercify.Utils.GlideApp;
 
 public class LikesActivity extends AppCompatActivity {
     private static final String TAG = "LikesActivity";
@@ -37,6 +45,22 @@ public class LikesActivity extends AppCompatActivity {
     private Context mContext = LikesActivity.this;
     private ImageView ivMap;
     private boolean mLocationPermissionGranted = false;
+
+    ListView simpleList;
+    String descList [] = {"has reported an incident!", "has reported an incident!", "has reported an incident!", "has reported an incident!", "has reported an incident!", "has reported an incident!", "has reported an incident!", "has reported an incident!", "has reported an incident!", "has reported an incident!"};
+    String timeList [] = {"666 hours ago", "666 hours ago", "666 hours ago", "666 hours ago", "666 hours ago", "666 hours ago", "666 hours ago", "666 hours ago", "666 hours ago", "666 hours ago"};
+    String countryList[] = {"Duterte", "Batman", "Flash", "Kuya Jobert", "Ferdinand", "Panot", "Pedro Pendukot", "Pokwang", "Superman", "Wonderwoman ko"};
+    int flags[] = {
+            R.drawable.duterte,
+            R.drawable.batman,
+            R.drawable.flash,
+            R.drawable.kuyajobert,
+            R.drawable.marcos,
+            R.drawable.noynoy,
+            R.drawable.pedro,
+            R.drawable.pokwang,
+            R.drawable.superman,
+            R.drawable.wonderwoman};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,22 +78,87 @@ public class LikesActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        simpleList = (ListView) findViewById(R.id.notif_listview);
+        CustomAdapter customAdapter = new CustomAdapter(getApplicationContext(), countryList, flags);
+        simpleList.setAdapter(customAdapter);
 
         setupBottomNavigationView();
-    }
 
+
+    }
 
 
     //Bottom Navigation View Setup
 
-    private void setupBottomNavigationView(){
+    private void setupBottomNavigationView() {
         Log.d(TAG, "setupBottomNavigationView: setting up bottom navigation view");
 
         BottomNavigationViewEx bottomNavigationViewEx = (BottomNavigationViewEx) findViewById(R.id.bottomNavViewBar);
         BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigationViewEx);
-        BottomNavigationViewHelper.enableNavigation(mContext,this,bottomNavigationViewEx);
+        BottomNavigationViewHelper.enableNavigation(mContext, this, bottomNavigationViewEx);
         Menu menu = bottomNavigationViewEx.getMenu();
         MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
         menuItem.setChecked(true);
+    }
+
+    private void loadImageByInternetUrl(){
+
+    }
+
+    public class CustomAdapter extends BaseAdapter {
+        Context context;
+        String countryList[];
+        int flags[];
+        LayoutInflater inflter;
+
+        public CustomAdapter(Context applicationContext, String[] countryList, int[] flags) {
+            this.context = context;
+            this.countryList = countryList;
+            this.flags = flags;
+            inflter = (LayoutInflater.from(applicationContext));
+        }
+
+        @Override
+        public int getCount() {
+            return countryList.length;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            view = inflter.inflate(R.layout.layout_notifs_listview, null);
+
+            TextView country = (TextView) view.findViewById(R.id.notif_text_name);
+            TextView desc = (TextView) view.findViewById(R.id.notif_text_desc);
+            TextView time = (TextView) view.findViewById(R.id.notif_text_time);
+            CircleImageView icon = (CircleImageView) view.findViewById(R.id.notif_list_item);
+
+            country.setText(countryList[i]);
+            desc.setText(descList[i]);
+            time.setText(timeList[i]);
+
+            String internetUrl = "http://futurefemaleleader.com/wp-content/uploads/2017/08/150508211850-kim-jong-un-sub-missile-test-0905-full-169_clipped_rev_1.png";
+
+            GlideApp
+                    .with(getApplicationContext())
+                    .load(internetUrl)
+                    .placeholder(R.mipmap.ic_emercify_launcher)
+                    .error(R.drawable.ic_error)
+                    .centerCrop()
+                    .into(icon);
+
+
+            return view;
+
+        }
     }
 }
