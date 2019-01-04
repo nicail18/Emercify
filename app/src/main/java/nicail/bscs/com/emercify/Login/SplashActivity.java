e675c05f8fd35c2ee4d9ac2463acd65a6e82d836
BonJoven <joven.montanez06@gmail.com>
2019-01-04 16:28:46 +0800

    splash screen and profile

--- app/src/main/java/nicail/bscs/com/emercify/Login/SplashActivity.java
+++ app/src/main/java/nicail/bscs/com/emercify/Login/SplashActivity.java
@@ -4,23 +4,60 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ProgressBar;
+import timber.log.Timber;
+
+import nicail.bscs.com.emercify.R;
+import timber.log.Timber;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private ProgressBar mProgress;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        mProgress = (ProgressBar) findViewById(R.id.splash_screen_progress_bar);
+//
+//        new Handler().postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+//                startActivity(intent);
+//                finish();
+//            }
+//        },1000);
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
+        // Show the splash screen
+        setContentView(R.layout.activity_splash);
+        mProgress = (ProgressBar) findViewById(R.id.splash_screen_progress_bar);
+
+        // Start lengthy operation in a background thread
+        new Thread(new Runnable() {
             public void run() {
-                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
-                startActivity(intent);
+                doWork();
+                startApp();
                 finish();
             }
-        },1000);
-
-
-
+        }).start();
     }
+
+    private void doWork() {
+        for (int progress=0; progress<100; progress+=10) {
+            try {
+                Thread.sleep(200);
+                mProgress.setProgress(progress);
+            } catch (Exception e) {
+                e.printStackTrace();
+                Timber.e(e.getMessage());
+            }
+        }
+    }
+
+    private void startApp() {
+        Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+
+
 }
