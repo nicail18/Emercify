ce746cb5d025c0f8d0c30b1d69e5c308ce97471e
Nicail Pepito <nicail180@gmail.com>
2019-01-05 23:17:50 +0800

    setting up blockchain

--- app/src/main/java/nicail/bscs/com/emercify/Utils/StringManipulation.java
+++ app/src/main/java/nicail/bscs/com/emercify/Utils/StringManipulation.java
@@ -1,5 +1,7 @@
 package nicail.bscs.com.emercify.Utils;
 
+import java.security.MessageDigest;
+
 public class StringManipulation {
 
     public static String expandUsername(String username){
@@ -33,5 +35,23 @@ public class StringManipulation {
             return s.substring(1, s.length());
         }
         return string;
+    }
+
+    public static String applySha256(String input){
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            //Applies sha256 to our input,
+            byte[] hash = digest.digest(input.getBytes("UTF-8"));
+            StringBuffer hexString = new StringBuffer(); // This will contain hash as hexidecimal
+            for (int i = 0; i < hash.length; i++) {
+                String hex = Integer.toHexString(0xff & hash[i]);
+                if(hex.length() == 1) hexString.append('0');
+                hexString.append(hex);
+            }
+            return hexString.toString();
+        }
+        catch(Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
